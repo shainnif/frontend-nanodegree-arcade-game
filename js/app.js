507a5b48f1bfea2dd871c0d3d8e778102fa39d92
Shainnif Ismail <shaine@carsift.co>
2018-03-20 17:54:23 +0000

    added comment for the inside function.

--- js/app.js
+++ js/app.js
@@ -1,9 +1,10 @@
 'use strict';
+
 /*
 This is over simplified a better solution might be to build a 2d array and maintain the location of each element there
 when user or bug attempts to enter the same square we have a collision.  The below functions and is confidently correct.
  */
-function inside(player, enemy){
+function inside(player, enemy) {
   return (player.x <= enemy.x + enemy.width && player.x + player.width >= enemy.x)
     && (player.y <= enemy.y + enemy.height && player.height + player.y >= enemy.y);
 }
@@ -20,9 +21,7 @@ var Enemy = function (x, y, speed) {
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-
-
+Enemy.prototype.update = function (dt) {
   if (this.x < 550) {
     this.x += dt * this.speed;
   } else {
@@ -33,25 +32,24 @@ Enemy.prototype.update = function(dt) {
 };
 
 // Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
+Enemy.prototype.render = function () {
   ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
 // Generate random speed for each enemy.
-Enemy.prototype.randomSpeed = function() {
-  this.speed = 80 * (Math.floor(Math.random() * 10) + 1);
+Enemy.prototype.randomSpeed = function () {
+  this.speed = 100 * (Math.random() * 10);
 };
 
 // This class requires an update(), render() and a handleInput() method.
-var Player = function() {
-  this.x = 202;
-  this.y = 380;
+var Player = function () {
+  this.reset();
   this.sprite = 'images/char-boy.png';
   this.height = 50; // Height and width for collision detection.
   this.width = 40;
 };
 
-Player.prototype.handleInput = function(keyCode) {
+Player.prototype.handleInput = function (keyCode) {
   // Switch statement to handle keyboard inputs and keep the player within game bounds.
   var xShift = 101;
   var yShift = 83;
@@ -67,7 +65,7 @@ Player.prototype.handleInput = function(keyCode) {
       }
       break;
     case 'up':
-      if (this.y > 0 ) {
+      if (this.y > 0) {
         this.y -= yShift; // engine.js
       }
       break;
@@ -79,7 +77,7 @@ Player.prototype.handleInput = function(keyCode) {
   }
 };
 
-Player.prototype.update = function() {
+Player.prototype.update = function () {
   if (this.y <= 10) {
     this.reset(202, 380);
   }
@@ -87,20 +85,20 @@ Player.prototype.update = function() {
 };
 
 // Render the player character on the screen.
-Player.prototype.render = function() {
+Player.prototype.render = function () {
   ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
 // Player position reset.
-Player.prototype.reset = function() {
+Player.prototype.reset = function () {
   this.x = 202;
   this.y = 380;
 };
 
 // Collision detection for enemy and player.
-Player.prototype.checkCollisions = function(){
-  allEnemies.forEach(function(enemy) {
-    if (inside(this, enemy)){
+Player.prototype.checkCollisions = function () {
+  allEnemies.forEach(function (enemy) {
+    if (inside(this, enemy)) {
       this.reset();
     }
   }.bind(this));
@@ -111,18 +109,16 @@ var player = new Player();
 
 var allEnemies = [];
 
-var enemyStartingPositions = [60, 143, 226]; // All possible Y locations for enemies.
+var enemyStartingPositions = [50, 140, 220]; // All possible Y locations for enemies.
 
 // Create enemies and push them into allEnemies array.
-enemyStartingPositions.forEach(function(y){
-  let x = Math.floor((Math.random() * 5 - 100));
-  let enemy = new Enemy(x, y);
-  allEnemies.push(enemy);
+enemyStartingPositions.forEach(function (y) {
+  allEnemies.push(new Enemy(-1, y));
 });
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function(e) {
+document.addEventListener('keyup', function (e) {
   var allowedKeys = {
     37: 'left',
     38: 'up',
